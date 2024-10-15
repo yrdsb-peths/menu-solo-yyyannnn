bc7bf02c1605e21ae452982dbff3186c77d2f3ff
yyyannnn <349168872@gapps.yrdsb.ca>
2024-10-15 13:21:46 -0400

    switch screens using buttons

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -1,22 +1,49 @@
 import greenfoot.*;
 
 public class InstructionScreen extends World {
+    
+    int numScreen = 0;
+    Label instruction;
+    String[] screen;
+
     public InstructionScreen() {
         super(600, 400, 1);
 
-        Label intruction1 = new Label("Screen 1", 50);
-        addObject(intruction1, 300, 200);
+        createScreens();
 
-        Button leftbutton = new leftButton(this::setScreen);
+        instruction = new Label(screen[numScreen], 50);
+        addObject(instruction, 300, 200);
+
+        Button leftbutton = new leftButton(this::leftScreen);
         addObject(leftbutton, 50, 300);
 
-        Button rightbutton = new rightButton(this::setScreen);
+        Button rightbutton = new rightButton(this::rightScreen);
         addObject(rightbutton, 550, 300);
 
     }
 
-    public void setScreen() {
+    public void act() {
+        setScreen();
+    }
 
+    public void leftScreen() {
+        numScreen--;
+    }
+
+    public void rightScreen() {
+        numScreen++;
+    }
+
+    public void createScreens() {
+        screen = new String[4];
+
+        for (int i = 0 ; i < screen.length ; i ++) {
+            screen[i] = "Screen " + (i+1);
+        }
+    }
+
+    public void setScreen() {
+        instruction.setValue(screen[numScreen]);
     }
 
     
