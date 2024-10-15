2f5c7fefc54ac17ddaef002b29f695a6d3d59123
yyyannnn <349168872@gapps.yrdsb.ca>
2024-10-15 12:47:58 -0400

    Added screen 1

--- MenuScreen.java
+++ MenuScreen.java
@@ -4,12 +4,11 @@ public class MenuScreen extends World {
     public MenuScreen() {
         super(600, 400, 1);
 
-        //Button button = new Button(this::goInstructions);
         Button button = new Button(this::goInstructions);
         addObject(button, 300, 340);
     }
 
     public void goInstructions() {
-        //Greenfoot.setWorld(new IntructionScreen(this));
+        Greenfoot.setWorld(new InstructionScreen());
     }
 }
